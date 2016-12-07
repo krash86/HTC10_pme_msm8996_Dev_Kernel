37471d825128194158a7230af660958bc319f2ac
Eliminater74 <eliminater74@gmail.com>
2016-12-07 16:31:51 -0500

    Nebula: Add My Personal Build Tools and Scripts
    Signed-off-by: Eliminater74 <eliminater74@gmail.com>

---
+++ make_defconfig2.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+DEFCONFIG_FILE=$1
+
+if [ -z "$DEFCONFIG_FILE" ]; then
+    echo "Need defconfig file(xxx_defconfig)!"
+    exit -1
+fi
+
+if [ ! -e arch/arm64/configs/$DEFCONFIG_FILE ]; then
+    echo "No such file : arch/arm64/configs/$DEFCONFIG_FILE"
+    exit -1
+fi
+
+# make .config
+env KCONFIG_NOTIMESTAMP=true \
+make ARCH=arm64 CROSS_COMPILE=/home/eliminater74/Builds/kernel/toolchains/LinaroTC/aarch64-linux-android-5.x-kernel/bin/aarch64-linux-android- ${DEFCONFIG_FILE}
+
+# run menuconfig
+env KCONFIG_NOTIMESTAMP=true \
+# make menuconfig ARCH=arm64
+make xconfig ARCH=arm64
+
+make savedefconfig ARCH=arm64
+# copy .config to defconfig
+mv defconfig arch/arm64/configs/${DEFCONFIG_FILE}
+# clean kernel object
+make mrproper
